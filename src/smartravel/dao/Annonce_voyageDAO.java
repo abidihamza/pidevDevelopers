b3f0f2d9ef0466cd9997e6243cec4bc3a78241d3
pc <pc@172.16.212.233>
2014-03-04 20:19:24 +0100

    Merge origin/master
    Conflicts:
    	src/smartravel/util/Connexion.java

--- src/smartravel/dao/Annonce_voyageDAO.java
+++ src/smartravel/dao/Annonce_voyageDAO.java
@@ -20,12 +20,14 @@ import java.util.List;
  */
 public class Annonce_voyageDAO {
        
+    public static int a=1;
         public void insertAnnonce(Voyage v) {
 
-        String requete = "INSERT INTO Voyage VALUES (?)";
+        String requete = "INSERT INTO Voyage VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement ps = Connexion.getInstance().prepareStatement(requete);
            System.out.println("xxxxxxxxxxx"); 
+            ps.setInt(1, a); 
             ps.setString(2, v.getMoyen_transport()); 
             ps.setString(3, v.getDestination());
             ps.setFloat(4, v.getBudget());
@@ -34,12 +36,15 @@ public class Annonce_voyageDAO {
             ps.setString(7, v.getItineraire());
             ps.setDate(8, new java.sql.Date(v.getDate_depart().getTime()));
             ps.setDate(9,new java.sql.Date(v.getDate_retour().getTime()));
+            ps.setInt(10, -1);
+            ps.setString(11, "Annonce");
             ps.executeUpdate();
             System.out.println("Ajout effectuée avec succès");
         } catch (SQLException ex) {
             //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("erreur lors de l'insertion " + ex.getMessage());
         }
+        a=a+1;
     }
         
          public void updateAnnonce(Voyage v) {
