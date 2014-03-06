ce23194fec0c6003e3190c2e0991d2bca9066e42
pc <pc@pc-PC>
2014-03-06 02:40:29 +0100

    Merge origin/master
    Conflicts:
    	src/smartravel/util/Connexion.java

--- src/smartravel/dao/Annonce_voyageDAO.java
+++ src/smartravel/dao/Annonce_voyageDAO.java
@@ -26,7 +26,7 @@ public class Annonce_voyageDAO {
         String requete = "INSERT INTO Voyage VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement ps = Connexion.getInstance().prepareStatement(requete);
-           System.out.println("xxxxxxxxxxx"); 
+           //pour detecter System.out.println("xxxxxxxxxxx"); 
             ps.setInt(1, a); 
             ps.setString(2, v.getMoyen_transport()); 
             ps.setString(3, v.getDestination());
@@ -113,8 +113,8 @@ public class Annonce_voyageDAO {
              
       public List<Voyage> DisplayAllVoyage() {
         List<Voyage> listeVoyage = new ArrayList<Voyage>();
-
-        String requete = "select * from voyage";
+        String Montype="Annonce";
+        String requete = " select * from voyage  where Type='"+Montype+"' ";
         try {
             Statement statement = Connexion.getInstance().createStatement();
             ResultSet resultat = statement.executeQuery(requete);
