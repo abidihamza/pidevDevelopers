b50de3b7fed156796226e48823c9254a45b5b348
pc <pc@pc-PC.mshome.net>
2014-03-07 00:24:04 +0100

    Merge origin/master
    Conflicts:
    	src/smartravel/util/Connexion.java

--- src/smartravel/gui/accueilForm.java
+++ src/smartravel/gui/accueilForm.java
@@ -5,10 +5,27 @@
  */
 
 package smartravel.gui;
-
+import javax.swing.*;
 import javax.swing.JOptionPane;
 import smartravel.dao.clientDao;
 import smartravel.entities.Voyageur;
+import smartravel.util.Connexion;
+import smartravel.entities.Voyage;
+import smartravel.dao.Annonce_voyageDAO;
+import javax.swing.table.DefaultTableModel; 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+import java.io.Console;
+import java.sql.*; 
+import java.util.HashSet; 
+import java.util.List;
+import java.util.Set; 
+import java.sql.Date;  //import java.util.Date;
+import smartravel.gui.AnnonceTable;
+import java.io.File;
+import javax.print.attribute.standard.Destination;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+
 
 /**
  *
@@ -22,6 +39,9 @@ public class accueilForm extends javax.swing.JFrame {
     public accueilForm() {
         super("Accueil de Smartravel");
         initComponents();
+        //@Lesannonces
+        AnnonceTable Tm=new AnnonceTable();
+        jTable1.setModel(Tm);
       
     }
     
@@ -55,6 +75,8 @@ public class accueilForm extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jPanel5 = new javax.swing.JPanel();
         jLabel8 = new javax.swing.JLabel();
 
@@ -63,10 +85,6 @@ public class accueilForm extends javax.swing.JFrame {
 
         jPanel3.setBackground(new java.awt.Color(153, 204, 255));
         jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
-
-        jLabel5.setIcon(new javax.swing.ImageIcon("E:\\Esprit\\2me Semestre\\logo2.jpg")); // NOI18N
-
-        jLabel4.setIcon(new javax.swing.ImageIcon("E:\\Esprit\\2me Semestre\\aa.jpg")); // NOI18N
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -169,8 +187,6 @@ public class accueilForm extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        jLabel7.setIcon(new javax.swing.ImageIcon("C:\\Users\\Belgacem\\Desktop\\beb.jpg")); // NOI18N
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -188,7 +204,7 @@ public class accueilForm extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(194, Short.MAX_VALUE)
                 .addComponent(jLabel7)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -212,6 +228,8 @@ public class accueilForm extends javax.swing.JFrame {
 
         jButton4.setText("Se connecter");
 
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
@@ -226,12 +244,15 @@ public class accueilForm extends javax.swing.JFrame {
                 .addGap(217, 217, 217)
                 .addComponent(jLabel6)
                 .addContainerGap(233, Short.MAX_VALUE))
+            .addComponent(jScrollPane1)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
                 .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 156, Short.MAX_VALUE)
+                .addGap(10, 10, 10)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
@@ -286,7 +307,7 @@ public class accueilForm extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 424, Short.MAX_VALUE)))
+                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(45, Short.MAX_VALUE))
         );
@@ -316,6 +337,7 @@ public class accueilForm extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
+        /*    
         clientDao cltdao = new clientDao() ;
        Voyageur voy    ;
        voy = cltdao.authentication(mail.getText());
@@ -328,7 +350,7 @@ public class accueilForm extends javax.swing.JFrame {
          vf.setVisible(true);
       
                    }else
-           JOptionPane.showMessageDialog(null, "invalid inseertion");     // TODO add your handling code here:
+           JOptionPane.showMessageDialog(null, "invalid inseertion");   */  // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void mailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mailActionPerformed
@@ -397,6 +419,8 @@ public class accueilForm extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField mail;
     private javax.swing.JPasswordField passwd;
     // End of variables declaration//GEN-END:variables
