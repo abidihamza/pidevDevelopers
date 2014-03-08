6e5037fa5a200c05ddb46001b66cde43f05e605b
Belgacem <Belgacem@172.16.210.253>
2014-03-08 17:49:09 +0100

    Merge origin/master
    Conflicts:
    	src/smartravel/dao/Annonce_voyageDAO.java
    	src/smartravel/gui/Inscription_Agence_Form.form
    	src/smartravel/gui/Inscription_Agence_Form.java

---
+++ src/smartravel/entities/ConfigUtility.java
@@ -0,0 +1,43 @@
+package smartravel.entities;
+ 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+ 
+/**
+ * A utility class that reads/saves SMTP settings from/to a properties file.
+ * @author www.codejava.net
+ *
+ */
+public class ConfigUtility {
+    private File configFile = new File("smtp.properties");
+    private Properties configProps;
+     
+    public Properties loadProperties() throws IOException {
+        Properties defaultProps = new Properties();
+        // sets default properties
+        defaultProps.setProperty("mail.smtp.host", "smtp.gmail.com");
+        defaultProps.setProperty("mail.smtp.port", "587");
+        defaultProps.setProperty("mail.user", "iskander.belgacem@gmail.com");
+        defaultProps.setProperty("mail.password", "besmelleh ");
+        defaultProps.setProperty("mail.smtp.starttls.enable", "true");
+        defaultProps.setProperty("mail.smtp.auth", "true");
+         
+        configProps = new Properties(defaultProps);
+         
+        // loads properties from file
+        if (configFile.exists()) {
+            InputStream inputStream = new FileInputStream(configFile);
+            configProps.load(inputStream);
+            inputStream.close();
+        }
+         
+        return configProps;
+    }
+     
+ 
+}
